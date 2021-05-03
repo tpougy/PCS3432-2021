723a9e9c2b7da4d3d771cf4fc9f24c2ecc91b01a
leoishicava <leoishicava@gmail.com>
2021-05-03 19:10:23 -0300

    Exp4

---
+++ Exp4/Relatorio/4_5_4_a.s
@@ -0,0 +1,32 @@
+@ gcc -o (nome) (arquivo).s para compilar
+@ gdb para rodar e debugar, Ctrl+x 2 para mudar vizualização, "b main" para breakpoint,
+@ r para dar run, s para step, c para continue,
+@ p/t $register -> printar valor do regs em binario
+@ p/x $register -> printar o valor do regs em hex
+@ x/x 0x??????? -> printar o conteudo da posicao ??????? da memoria em hex
+
+    .text
+ 	.globl main
+main:
+    ADR     r0, array           @ array a
+    LDR     r1, =0x4            @ define s (constante arbitrária)
+    LDR     r2, =0              @ inicializa i
+    MOV     r3, #0              @ reg auxiliar guarda 0
+
+    B       forheader
+
+forheader:
+    CMP     r2, r1              @ compara i com s
+    BLT     forloop             @ se i < s entra no loop
+    B       break
+
+forloop:
+    STRB    r3, [r0, r2]        @ mem(r0+r2) recebe r3 -> mem(array[0+i]) recebe 0
+    ADD     r2, r2, #1          @ incrementa i de 1 unidade
+    B forheader
+
+break:
+    SWI     0x123456            @ encerra programa
+
+array: .byte 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+
---
+++ Exp4/Relatorio/4_5_4_b.s
@@ -0,0 +1,35 @@
+@ gcc -o (nome) (arquivo).s para compilar
+@ gdb para rodar e debugar, Ctrl+x 2 para mudar vizualização, "b main" para breakpoint,
+@ r para dar run, s para step, c para continue,
+@ p/t $register -> printar valor do regs em binario
+@ p/x $register -> printar o valor do regs em hex
+@ x/x 0x??????? -> printar o conteudo da posicao ??????? da memoria em hex
+
+    .text
+ 	.globl main
+main:
+    ADR     r0, array           @ array a
+    LDR     r1, =0x4            @ define s (constante arbitrária)
+    MOV     r10, #0             @ reg auxiliar guarda 0
+    B       forinit
+
+forinit:
+    MOV     r2, r0              @ inicializa p
+    ADD     r11, r0, r1         @ inicializa um reg auxiliar para a comparação com p
+    B       forheader 
+
+forheader:
+    CMP     r2, r11             @ compara p com &array[s]
+    BLT     forloop             @ se p < &array[s] entra no loop
+    B       break
+
+forloop:
+    STRB    r10, [r2]           @ mem(r2) recebe 0
+    ADD     r2, r2, #1          @ incremente o ponteiro p de 1 posicao
+    B forheader
+
+break:
+    SWI     0x123456           @ encerra programa
+
+array: .byte 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+
