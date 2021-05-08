5d20a6e5c0176b7856080effb21219305fba6f02
leoishicava <leoishicava@gmail.com>
2021-05-08 09:59:00 -0300

    Exp5 Sab 08/05/21

--- Exp5/Planejamento/C.s
+++ Exp5/Planejamento/C.s
@@ -8,27 +8,18 @@
         .text
         .globl main
 main: 
-        LDR     r0, =0b10110001101100101001010101101100 @ sequencia  
+        LDR     r0, =0b10110001101100101001010101101100  @ sequencia  
         MOV     r8, r0                  @ copia o conteudo de r0 para r8
         LDR     r1, =0x0                @ variavel i
         LDR     r2, =0x0                @ saida
         LDR     r3, =0x0                @ reg q guarda o bit a ser analisado
-        LDR     r9, =0x20                @ tamanho da sequencia 
+        LDR     r9, =0x20               @ tamanho da sequencia 
 
         RSB     r7, r9, #32             @ r7 = 32 - r9
         MOV     r0, r0, LSL r7          @ shift left de (32-r9) bits
 
 primeiro:
-        CMP     r1, r9                  @ compara r1 com r9
-        BGE     fim                     @ se r1 >= r9, vai para fim
-
-        MOVS    r0, r0, LSL #1          @ adiciona o primeiro bit no carry
-        LDR     r3, =0x0                @ reseta o conteudo de r3
-        ADC     r3, r3, #0              @ adiciona carry em r3
-
-        MOV     r2, r2, LSL #1          @ coloca 0 na saida
-
-        ADD     r1, r1, #1              @ i++
+        BL      inicio_estado           @ vai para inicio_estado
 
         CMP     r3, #1                  @ compara r3 com 1
         BEQ     segundo                 @ se r3 = 1, vai para o segundo estado
@@ -36,16 +27,7 @@ primeiro:
         B       primeiro                @ volta para primeiro estado
 
 segundo:
-        CMP     r1, r9                  @ compara r1 com r9
-        BGE     fim                     @ se r1 >= r9, vai para fim
-
-        MOVS    r0, r0, LSL #1          @ adiciona o primeiro bit no carry
-        LDR     r3, =0x0                @ reseta o conteudo de r3
-        ADC     r3, r3, #0              @ adiciona carry em r3
-
-        MOV     r2, r2, LSL #1          @ coloca 0 na saida
-
-        ADD     r1, r1, #1              @ i++
+        BL      inicio_estado           @ vai para inicio_estado
         
         CMP     r3, #0                  @ compara r3 com 1
         BEQ     terceiro                @ se r3 = 1, vai para o terceiro estado
@@ -53,6 +35,26 @@ segundo:
         B       segundo                 @ volta para loop
 
 terceiro:
+        BL      inicio_estado           @ vai para inicio_estado
+        
+        CMP     r3, #1                  @ compara r3 com 0
+        BEQ     quarto                  @ se r3 = 0, vai para o quarto estado
+
+        B       primeiro                @ volta para loop
+
+quarto:
+        BL      inicio_estado           @ vai para inicio_estado
+        
+        CMP     r3, #1                  @ compara r3 com 1
+        ADDEQ   r2, r2, #1              @ se r3 = 1, atingiu a sequencia, coloca 1 na saida
+        BEQ     segundo                 @ se r3 = 1, volta para segundo estado
+
+        B       terceiro                @ volta para segundo, visto que pode ser o comeco da prox sequencia
+
+fim:
+        SWI     0x123456                @ encerra programa
+
+inicio_estado:
         CMP     r1, r9                  @ compara r1 com r9
         BGE     fim                     @ se r1 >= r9, vai para fim
 
@@ -63,29 +65,5 @@ terceiro:
         MOV     r2, r2, LSL #1          @ coloca 0 na saida
 
         ADD     r1, r1, #1              @ i++
-        
-        CMP     r3, #1                  @ compara r3 com 0
-        BEQ     quarto                  @ se r3 = 0, vai para o quarto estado
 
-        B       primeiro                @ volta para loop
-
-quarto:
-        CMP     r1, r9                  @ compara r1 com r9
-        BGE     fim                     @ se r1 >= r9, vai para fim
-
-        MOVS    r0, r0, LSL #1          @ adiciona o primeiro bit no carry
-        LDR     r3, =0x0                @ reseta o conteudo de r3
-        ADC     r3, r3, #0              @ adiciona carry em r3
-
-        MOV   r2, r2, LSL #1
-
-        ADD     r1, r1, #1              @ i++
-        
-        CMP     r3, #1                  @ compara r3 com 1
-        ADDEQ   r2, r2, #1              @ se r3 = 1, atingiu a sequencia, shift left em r2, e coloca 1 na saida
-        BEQ     segundo
-
-        B       terceiro                @ volta para segundo, visto que pode ser o comeco da prox sequencia
-
-fim:
-        SWI     0x123456                @ encerra programa
+        MOV     pc, lr                  @ volta para a funcao
